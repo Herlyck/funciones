d124c7aeeefbb5ac03455d134e5824dca32729f3
Herlyck <mrcyrax64@gmail.com>
2026-06-15 16:16:56 -0300

    actualizado

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# funciones
+# funcion
+
+const leer = require("prompt-sync")();
+function main(params) {
+
+    let segundos = -1;
+    segundos = 3;
+
+
+    console.log(segundos); //mostrar antes de que comience el intervalo
+    const idInterval = setInterval(() => {
+        //codigo
+        segundos--;
+        console.log(segundos);
+
+        if (segundos === 0) {
+            clearInterval(idInterval); // la forma en que termina el intervalo, necesita el argumento del intervalo a detener
+        }
+
+    }, 1000);  //1000 mseg = 1seg
+
+
+
+
+
+
+}
+main();
--- funcion3.js
+++ funcion3.js
@@ -1,19 +1,32 @@
-const leer = require ("prompt-sync")();
+const leer = require("prompt-sync")();
 function main(params) {
-    
-    let contador=0;
-    let valor = 0;
 
-    console.log("ingrese valor:");
-    contador=Number(leer());
-    
-    cuentaRegresivaDesde(contador);
+    let segundos = -1;
 
-
+    console.log("ingresa el segundo donde iniciara la cuenta regresiva");
+    segundos = Number(leer());
     
+    
+    cuentaRegresivaDesde(segundos); //este segundos no esta en el otro modulo entonces lo envio con el mismo nombre y lo modifico con f2
+
 }
-main();
+main(); //funcion main (activa subalgoritmos)
 
-function cuentaRegresivaDesde(contador) {
-    console.log(contador-1);
+
+                                //le modificas a segundos con F2 por segundosInicial
+function cuentaRegresivaDesde(segundosInicial) {
+
+    console.log("iniciando cuenta regresiva...");
+    console.log(segundosInicial); //mostrar antes de que comience el intervalo
+    
+    const idInterval = setInterval(() => {
+        //codigo
+        segundosInicial--;
+        console.log(segundosInicial); 
+
+        if (segundosInicial === 0) {
+            clearInterval(idInterval); // la forma en que termina el intervalo, necesita el argumento del intervalo a detener
+        }
+
+    }, 1000);  //1000 mseg = 1seg
 }
