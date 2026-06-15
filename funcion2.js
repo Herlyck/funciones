14edb72509ab144f5c380e1506475e4feac7c20e
Herlyck <mrcyrax64@gmail.com>
2026-06-15 15:35:09 -0300

    actualizado

--- funcion2.js
+++ funcion2.js
@@ -8,6 +8,11 @@ function main() {
 }
 main();
 
+/**
+ * saluda a una persona con su nombre y un mensaje
+ * @param {String} nombre este parametro se usa para saludar con un mensaje
+ */
+
 function nombreSaludar(nombre) {
     console.log(`que tengas un buen dia ${nombre}`);
     
